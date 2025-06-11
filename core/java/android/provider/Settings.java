e1ee75c2e044d653e4b5720c62d0e55a204aa1ed
Abhay Singh Gill <abhaygill017@gmail.com>
2025-06-11 15:41:40 +0530

    base: Allow user to provide keybox data [1/2]
    Handle PKCS#1 RSA keys for keybox spoofing as well
    
    Change-Id: Ia76f2a06f17e295dffef44312295fab9bd67839f
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10989,6 +10989,13 @@ public final class Settings {
         public static final String ADAPTIVE_CONNECTIVITY_ENABLED = "adaptive_connectivity_enabled";
 
         /**
+         * User selectable keybox data.
+         * @hide
+         */
+        @Readable
+        public static final String KEYBOX_DATA = "keybox_data";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

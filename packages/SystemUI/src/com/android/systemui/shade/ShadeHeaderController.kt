OrionOS-Project/frameworks_base

Fix carrier name animation during qqs expand
The bug is a result of two recent changes: ag/20943168 and ag/22645423.

The fix is very simple and sets the startPadding of the carrier group to the max width of the clock (when fully scaled) no matter the actual scale of the clock. As a result, the carrier group will never overlap the clock and it will also not move during the qqs expand animation. Since the collapsed state of the combined_qs_header is only there for animation purposes (as far as I understand) and is always faded out when collapsed, this change should not have any negative effects.

Bug: 280016031
Test: atest ShadeHeaderControllerTest, Manual, i.e. testing the qqs expand and collapse animation with no carrier, one carrier, multiple carriers and rtl.
Change-Id: Ie4745bd466fb1a24dcd47d6f85c545764d6c8662

packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -34,6 +34,7 @@ import android.view.WindowInsets
 import android.widget.TextView
 import androidx.annotation.VisibleForTesting
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.doOnLayout
 import com.android.app.animation.Interpolators
 import com.android.settingslib.Utils
 import com.android.systemui.Dumpable
@@ -220,6 +221,7 @@ constructor(
             override fun demoCommands() = listOf(DemoMode.COMMAND_CLOCK)
             override fun dispatchDemoCommand(command: String, args: Bundle) =
                 clock.dispatchDemoCommand(command, args)
+
             override fun onDemoModeStarted() = clock.onDemoModeStarted()
             override fun onDemoModeFinished() = clock.onDemoModeFinished()
         }
@@ -259,6 +261,7 @@ constructor(
                     resources.getDimensionPixelSize(R.dimen.large_screen_shade_header_min_height)
                 lastInsets?.let { updateConstraintsForInsets(header, it) }
                 updateResources()
+                updateCarrierGroupPadding()
             }
         }
 
@@ -291,6 +294,7 @@ constructor(
         privacyIconsController.chipVisibilityListener = chipVisibilityListener
         updateVisibility()
         updateTransition()
+        updateCarrierGroupPadding()
 
         header.setOnApplyWindowInsetsListener(insetListener)
 
@@ -298,8 +302,6 @@ constructor(
             val newPivot = if (v.isLayoutRtl) v.width.toFloat() else 0f
             v.pivotX = newPivot
             v.pivotY = v.height.toFloat() / 2
-
-            mShadeCarrierGroup.setPaddingRelative((v.width * v.scaleX).toInt(), 0, 0, 0)
         }
         clock.setOnClickListener { launchClockActivity() }
 
@@ -357,6 +359,14 @@ constructor(
         header
             .getConstraintSet(LARGE_SCREEN_HEADER_CONSTRAINT)
             .load(context, resources.getXml(R.xml.large_screen_shade_header))
+    }
+
+    private fun updateCarrierGroupPadding() {
+        clock.doOnLayout {
+            val maxClockWidth =
+                (clock.width * resources.getFloat(R.dimen.qqs_expand_clock_scale)).toInt()
+            mShadeCarrierGroup.setPaddingRelative(maxClockWidth, 0, 0, 0)
+        }
     }
 
     private fun updateConstraintsForInsets(view: MotionLayout, insets: WindowInsets) {

LineageOS-T/android_frameworks_base

packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -36,6 +36,7 @@ import android.view.WindowInsets
 import android.widget.TextView
 import androidx.annotation.VisibleForTesting
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.doOnLayout
 import com.android.settingslib.Utils
 import com.android.systemui.Dumpable
 import com.android.systemui.R
@@ -262,6 +263,7 @@ constructor(
 
             override fun onUiModeChanged() {
                 updateResources()
+                updateCarrierGroupPadding()
             }
         }
 
@@ -301,6 +303,7 @@ constructor(
         privacyIconsController.chipVisibilityListener = chipVisibilityListener
         updateVisibility()
         updateTransition()
+        updateCarrierGroupPadding()
 
         header.setOnApplyWindowInsetsListener(insetListener)
 
@@ -308,8 +311,6 @@ constructor(
             val newPivot = if (v.isLayoutRtl) v.width.toFloat() else 0f
             v.pivotX = newPivot
             v.pivotY = v.height.toFloat() / 2
-
-            qsCarrierGroup.setPaddingRelative((v.width * v.scaleX).toInt(), 0, 0, 0)
         }
 
         dumpManager.registerDumpable(this)
@@ -355,6 +356,14 @@ constructor(
         header
             .getConstraintSet(LARGE_SCREEN_HEADER_CONSTRAINT)
             .load(context, resources.getXml(R.xml.large_screen_shade_header))
+    }
+
+    private fun updateCarrierGroupPadding() {
+        clock.doOnLayout {
+            val maxClockWidth =
+                (clock.width * resources.getFloat(R.dimen.qqs_expand_clock_scale)).toInt()
+            qsCarrierGroup.setPaddingRelative(maxClockWidth, 0, 0, 0)
+        }
     }
 
     private fun updateConstraintsForInsets(view: MotionLayout, insets: WindowInsets) {
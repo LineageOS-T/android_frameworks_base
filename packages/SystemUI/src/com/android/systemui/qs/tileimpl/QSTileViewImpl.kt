ee546245a47b3e0a487a3e4fb60f96a805f338aa
neophyte404 <260553983+neophyte404@users.noreply.github.com>
2026-07-13 18:02:49 +0000

    Revert "SystemUI: Add settings to change QS tile shape [1/2]"
    This reverts commit 623c7853fa38b50af4b9784ea9a74e2b49c3e090.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -28,7 +28,6 @@ import android.graphics.drawable.Drawable
 import android.graphics.drawable.RippleDrawable
 import android.graphics.drawable.GradientDrawable
 import android.os.Trace
-import android.provider.Settings
 import android.service.quicksettings.Tile
 import android.text.TextUtils
 import android.util.Log
@@ -180,7 +179,7 @@ open class QSTileViewImpl @JvmOverloads constructor(
         background = null
 
         val iconContainerSize = context.resources.getDimensionPixelSize(R.dimen.qs_quick_tile_size)
-        radiusActive = iconContainerSize / 3f
+        radiusActive = iconContainerSize / 2f
         radiusInactive = iconContainerSize / 4f
         iconContainer = LinearLayout(context)
         iconContainer.layoutParams = LayoutParams(iconContainerSize, iconContainerSize)
@@ -622,13 +621,6 @@ open class QSTileViewImpl @JvmOverloads constructor(
         mBg.cornerRadius = cornerRadius
     }
     private fun getCornerRadiusForState(state: Int): Float {
-        var qsTileShape : Int = Settings.Secure.getInt(context.contentResolver, Settings.Secure.QS_TILE_SHAPE, 2)
-        if (qsTileShape == 0)
-            return radiusActive
-
-        if (qsTileShape == 1)
-            return radiusInactive
-
         return when (state) {
             Tile.STATE_ACTIVE -> radiusActive
             Tile.STATE_INACTIVE -> radiusInactive
